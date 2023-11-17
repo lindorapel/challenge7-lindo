0a50043dc2327af4cbd16d310e0f93f35aa00129
lindorapel <lindorapel@gmail.com>
2023-11-17 15:06:55 +0700

    edit mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducers from "./reducers";
-import thunk from "redux-thunk"
+import thunk from "redux-thunk";
 
 // create the store or temporary state database
 export default configureStore({
-    reducer: rootReducers,
-    devTools: import.meta.env.MODE === "development",
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // redux thunk
+  reducer: rootReducers,
+  devTools: import.meta.env.VITE_MODE !== "production",
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // redux thunk
 });
